bf4e5c6501e44c52f73df584daf8abb5c405ede1
fhkathika <fhkathika057@gmail.com>
2026-05-15 22:52:50 +0600

    create server with express

---
+++ src/server.ts
@@ -0,0 +1,55 @@
+import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
+const app:Application = express()
+const port = 5000
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended:true}))
+const pool=new Pool({
+    connectionString:"postgresql://neondb_owner:npg_q4nijLOEf1RS@ep-falling-lab-aqz1hhra-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+})
+const initDB=async()=>{
+    try{
+await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    email VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    is_active BOOLEAN DEFAULT true,
+    age INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+
+    )
+    `)
+    console.log("DB connected successfully")
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+initDB()
+app.get('/', (req:Request, res:Response) => {
+//   res.send('Hello World')
+res.status(200).json({
+    "message":"Express Server",
+    "author":"next level",
+
+})
+})
+app.post('/',async(req:Request,res:Response)=>{
+console.log(req.body)
+const {name,email,password}=req.body
+res.status(201).json({
+    message:"created",
+    data:{
+        name,email
+    },
+})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
