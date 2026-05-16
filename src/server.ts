0e6557df25f0ca14e8e1c4c85011774a4750e142
fhkathika <fhkathika057@gmail>
2026-05-16 16:48:52 +0600

    module 7 complete

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express, { type Application, type Request, type Response } from "express"
 import {Pool} from "pg"
+import config from "./config"
 const app:Application = express()
-const port = 5000
+const port = config.port
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended:true}))
 const pool=new Pool({
-    connectionString:"postgresql://neondb_owner:npg_q4nijLOEf1RS@ep-falling-lab-aqz1hhra-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    // connectionString:config.connection_string,
+    connectionString:"postgresql://neondb_owner:npg_q4nijLOEf1RS@ep-falling-lab-aqz1hhra-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  
 
 })
 const initDB=async()=>{
@@ -15,7 +18,7 @@ await pool.query(`
     CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(20),
-    email VARCHAR(20) NOT NULL,
+    email VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL,
     is_active BOOLEAN DEFAULT true,
     age INT,
@@ -39,15 +42,144 @@ res.status(200).json({
 
 })
 })
-app.post('/',async(req:Request,res:Response)=>{
-console.log(req.body)
-const {name,email,password}=req.body
+// add user 
+app.post('/api/users',async(req:Request,res:Response)=>{
+
+const {name,email,password,age}=req.body
+try{
+const result=await pool.query(`
+    INSERT INTO users(name,email,password,age)
+    VALUES($1,$2,$3,$4)
+    RETURNING *
+    `,[name,email,password,age])
+
 res.status(201).json({
-    message:"created",
-    data:{
-        name,email
-    },
+    message:"user created successfully",
+    data:result.rows[0],
 })
+}
+catch(error:any){
+res.status(500).json({
+    message:error.message,
+    error:error,
+})
+}
+})
+// get all user 
+app.get('/api/user',async(REQ:Request,res:Response)=>{
+try{
+const result=await pool.query(`
+    SELECT * FROM users
+    `)
+    res.status(200).json({
+        success:true,
+        message:"Users,retrived successfully",
+        data:result.rows
+    })
+}
+catch(err:any){
+ res.status(500).json({
+        success:false,
+        message:err.message,
+        error:err
+    })
+}
+})
+// get single user 
+app.get('/api/user/:id',async(req:Request,res:Response)=>{
+const {id}=req.params;
+console.log(id)
+try{
+const result=await pool.query(`
+    SELECT * FROM users WHERE id=$1
+    `,[id])
+      if(result.rows.length===0){
+        res.status(404).json({
+            success:false,
+    message:"Users not found",
+    data:{}
+})
+    }
+      res.status(200).json({
+        success:true,
+        message:"User,retrived successfully",
+        data:result.rows[0]
+    })
+  
+}
+catch(error:any){
+res.status(500).json({
+    message:error.message,
+    error:error,
+})
+}
+})
+//put method 
+app.put('/api/users/:id',async(req:Request,res:Response)=>{
+const {id}=req.params;
+const {name,password,age,is_active}=req.body;
+// console.log("Id:",id)
+// console.log({name,password,age,is_active})
+try{
+const result=await pool.query(`
+    UPDATE users 
+    SET 
+    name=COALESCE($1 ,name),
+    password=COALESCE($2, password),
+    age=COALESCE($3 ,age),
+    is_active=COALESCE($4 ,is_active)
+    WHERE id=$5 RETURNING *
+    `,[name,password,age,is_active,id]
+)
+if(result.rows.length===0){
+    res.status(404).json({
+        success:false,
+        message:"User not found"
+    })
+}
+ res.status(200).json({
+        success:true,
+        message:"User,updated successfully",
+        data:result.rows[0]
+    })
+}
+catch(err:any){
+ res.status(500).json({
+        success:false,
+        message:err.message,
+        error:err
+    })
+}
+
+})
+
+// delete user
+
+app.delete('/api/users/:id',async(req:Request,res:Response)=>{
+const {id}=req.params;
+try{
+const result=await pool.query(`
+    DELETE FROM users WHERE id=$1
+    `,[id])
+    if(result.rowCount===0){
+            res.status(404).json({
+        success:false,
+        message:"User not found"
+    })
+    }
+    res.status(200).json({
+        success:true,
+        message:"User deleted successfully",
+        data:{}
+    })
+}
+catch(err:any){
+    res.status(500).json({
+        success:false,
+        message:err.message,
+        error:err
+    }) 
+}
 })
 
 app.listen(port, () => {
