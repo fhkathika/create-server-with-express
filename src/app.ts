26de004573a9535ceb5521f3bf8476136c04f346
fhkathika <fhkathika057@gmail>
2026-05-23 11:59:06 +0600

    push older code

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,20 @@ import { pool } from "./db"
 import { userRoute } from "./modules/user/user.route"
 import { profileROute } from "./modules/profile/profile.route"
 import { authRouter } from "./modules/auth/auth.route"
+import fs from "fs"
 const app:Application = express()
 
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended:true}))
-
+app.use((req, res, next) => {
+  console.log('Method-URL-Time:', req.method,req.url,Date.now())
+ const log=`\nMethd->${ req.method} Time ->${Date.now()}URL${req.url}\n`
+ fs.appendFile("logger Text",log,(err)=>{
+console.log(err)
+ }) 
+ next()
+})
 
 app.get('/', (req:Request, res:Response) => {
 //   res.send('Hello World')
@@ -21,7 +29,7 @@ res.status(200).json({
 })
 })
 app.use('/api/user',userRoute)
-
 app.use("/api/profile",profileROute)
 app.use("/api/auth",authRouter);
+
 export default app
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,7 @@
+import bcrypt from "bcryptjs";
 import { pool } from "../../db";
-
+import jwt from "jsonwebtoken" 
+import config from "../../config";
 const loginUserIntoDB=async(payload:{
     email:string;
     password:string;
@@ -11,8 +13,26 @@ const {email,password}=payload;
 const userData=await pool.query(`
     
     SELECT * FROM users WHERE email=$1`,[email]);
+    if(userData.rows.length===0){
+        throw new Error("Invalid credintial!")
+    }
     const user=userData.rows[0]
-    console.log("user",user)
+  const matchPassword=await bcrypt.compare(password,user.password)
+  console.log("matchPassword",matchPassword)
+  if(!matchPassword){
+       throw new Error("Invalid credintial!")
+  }
+//   generate token 
+const jwtpayload={
+    id:user.id,
+    name:user.name,
+    is_active:user.is_active,
+    email:user.email
+}
+const accessToken=jwt.sign(jwtpayload,config.secret as string,{
+    expiresIn:"1d"
+})
+return {accessToken}
 }
 export const authService={
     loginUserIntoDB
